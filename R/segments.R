ca7b2f3254e16e08f32ecc08be0be090e546e8f1
Kirsten Van Hoorde <kirsten.vanhoorde@openanalytics.eu>
2015-11-30 14:39:08 +0100

    Modification to function pca.segments.2d (bug)

--- R/segments.R
+++ R/segments.R
@@ -207,7 +207,7 @@ pca.segments.2D <- function(.data, .cast.freq.seg = T, ..., .text = T, .do.plot 
     pca.res <- data.frame(PC1 = pca.res$x[,1], PC2 = pca.res$x[,2], Subject = names(.data))
     p <- ggplot() + geom_point(aes(x = PC1, y = PC2, colour = Subject), size = 3, data = pca.res)
     if (.text) {
-      p <- geom_text(aes(x = PC1, y = PC2, label = Subject), data = pca.res, hjust=.5, vjust=-.3)
+      p <- p + geom_text(aes(x = PC1, y = PC2, label = Subject), data = pca.res, hjust=.5, vjust=-.3)
     }
     p <- p + theme_linedraw() + guides(size=F) + ggtitle("VJ-usage: Principal Components Analysis") + .colourblind.discrete(length(pca.res$Subject), T)
   } else {
