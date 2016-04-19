3adf53032b2660ed84587f4885afb98303aa8a3d
Vadim Nazarov <vdn@mailbox.org>
2016-04-19 19:55:28 +0300

    IG segments for homo sapies

--- R/docdata.R
+++ R/docdata.R
@@ -180,54 +180,82 @@ HUMAN_TRBV_ALS <- .set.attr(HUMAN_TRBV_ALS, "column", "V.allele")
 # HUMAN_TRBV_GEN <- c()
 # HUMAN_TRBV_ALS <- c()
 
-HUMAN_TRDD = c('TRDD1', 'TRDD2')
+HUMAN_TRDD = c('TRDD1', 'TRDD2', 'TRDD3')
 
-HUMAN_TRDJ = c('TRDJ1', 'TRDJ2')
+HUMAN_TRDJ = c('TRDJ1', 'TRDJ3', 'TRDJ4', 'TRDJ2')
 
-HUMAN_TRDV = c('TRAV6-7/DV9', 'TRAV13-4/DV7', 'TRAV21/DV12', 'TRDV2-1', 'TRAV15-1/DV6-1', 'TRAV15D-2/DV6D-2', 
-               'TRDV1', 'TRDV4', 'TRAV14D-3/DV8', 'TRDV2-2', 'TRAV15D-1/DV6D-1', 
-               'TRAV16D/DV11', 'TRAV15-2/DV6-2', 'TRDV5', 'TRAV4-4/DV10')
+HUMAN_TRDV = c('TRDV3', 'TRAV38-2/DV8', 'TRDV2', 'TRAV23/DV6', 'TRAV29/DV5', 'TRAV36/DV7', 
+               'TRAV14/DV4', 'TRDV1')
 
-HUMAN_TRGJ = c('TRGJ4', 'TRGJ2', 'TRGJ1', 'TRGJ3')
+HUMAN_TRGJ = c('TRGJ1', 'TRGJP1', 'TRGJ2', 'TRGJP', 'TRGJP2')
 
-HUMAN_TRGV = c('TRGV5', 'TRGV1', 'TRGV4', 'TRGV3', 'TRGV7', 'TRGV2', 
-               'TRGV6')
+HUMAN_TRGV = c('TRGV5P', 'TRGV10', 'TRGV5', 'TRGV3', 'TRGVA', 'TRGV8', 
+               'TRGV4', 'TRGV11', 'TRGV2', 'TRGV9', 'TRGV1')
 
-HUMAN_IGHV <- c('IGHV1-18', 'IGHV1-2', 'IGHV1-24', 'IGHV1-3', 'IGHV1-45', 'IGHV1-46', 'IGHV1-58', 'IGHV1-69', 
-                'IGHV1-69-2', 'IGHV1-69D', 'IGHV1-8', 'IGHV1-NL1', 'IGHV1/OR15-1', 'IGHV1/OR15-2', 'IGHV1/OR15-3', 
-                'IGHV1/OR15-4', 'IGHV1/OR15-5', 'IGHV1/OR15-9', 'IGHV1/OR21-1', 'IGHV2-10', 'IGHV2-26', 'IGHV2-5', 
-                'IGHV2-70', 'IGHV2/OR16-5', 'IGHV3-11', 'IGHV3-13', 'IGHV3-15', 'IGHV3-16', 'IGHV3-19', 'IGHV3-20',
-                'IGHV3-21', 'IGHV3-22', 'IGHV3-23', 'IGHV3-23D', 'IGHV3-25', 'IGHV3-30', 'IGHV3-30-3', 'IGHV3-33', 
-                'IGHV3-35', 'IGHV3-38', 'IGHV3-38-3', 'IGHV3-43', 'IGHV3-43D', 'IGHV3-47', 'IGHV3-48', 'IGHV3-49', 
-                'IGHV3-52', 'IGHV3-53', 'IGHV3-54', 'IGHV3-62', 'IGHV3-63', 'IGHV3-64', 'IGHV3-66', 'IGHV3-69-1', 
-                'IGHV3-7', 'IGHV3-71', 'IGHV3-72', 'IGHV3-73', 'IGHV3-74', 'IGHV3-9', 'IGHV3-NL1', 'IGHV3/OR15-7', 
-                'IGHV3/OR16-10', 'IGHV3/OR16-12', 'IGHV3/OR16-13', 'IGHV3/OR16-14', 'IGHV3/OR16-15', 'IGHV3/OR16-16', 
-                'IGHV3/OR16-8', 'IGHV3/OR16-9', 'IGHV4-28', 'IGHV4-30-2', 'IGHV4-30-4', 'IGHV4-31', 'IGHV4-34', 'IGHV4-38-2', 
-                'IGHV4-39', 'IGHV4-4', 'IGHV4-55', 'IGHV4-59', 'IGHV4-61', 'IGHV4/OR15-8', 'IGHV5-10-1', 'IGHV5-51', 
-                'IGHV5-78', 'IGHV6-1', 'IGHV7-34-1', 'IGHV7-4-1', 'IGHV7-81')
-HUMAN_IGHD <- c('IGHD1-1', 'IGHD1-14', 'IGHD1-20', 'IGHD1-26', 'IGHD1-7', 'IGHD1/OR15-1a', 'IGHD1/OR15-1b', 'IGHD2-15', 
-                'IGHD2-2', 'IGHD2-21', 'IGHD2-8', 'IGHD2/OR15-2a', 'IGHD2/OR15-2b', 'IGHD3-10', 'IGHD3-16', 'IGHD3-22', 
-                'IGHD3-3', 'IGHD3-9', 'IGHD3/OR15-3a', 'IGHD3/OR15-3b', 'IGHD4-11', 'IGHD4-17', 'IGHD4-23', 'IGHD4-4', 
-                'IGHD4/OR15-4a', 'IGHD4/OR15-4b', 'IGHD5-12', 'IGHD5-18', 'IGHD5-24', 'IGHD5-5', 'IGHD5/OR15-5a', 
-                'IGHD5/OR15-5b', 'IGHD6-13', 'IGHD6-19', 'IGHD6-25', 'IGHD6-6', 'IGHD7-27')
-HUMAN_IGHJ <- c('IGHJ1', 'IGHJ2', 'IGHJ3', 'IGHJ4', 'IGHJ5', 'IGHJ6')
+HUMAN_IGHD = c('IGHD4-4', 'IGHD4-23', 'IGHD2-2', 'IGHD1-7', 'IGHD3-3', 'IGHD5-18', 
+               'IGHD4/OR15-4a', 'IGHD2-21', 'IGHD4/OR15-4b', 'IGHD1/OR15-1a', 'IGHD1-14', 
+               'IGHD6-13', 'IGHD5-24', 'IGHD2/OR15-2a', 'IGHD5-12', 'IGHD6-19', 
+               'IGHD4-11', 'IGHD5-5', 'IGHD1/OR15-1b', 'IGHD3/OR15-3a', 'IGHD4-17', 
+               'IGHD6-25', 'IGHD1-1', 'IGHD3-16', 'IGHD2-8', 'IGHD3-10', 
+               'IGHD3/OR15-3b', 'IGHD5/OR15-5a', 'IGHD3-9', 'IGHD7-27', 'IGHD1-20', 
+               'IGHD1-26', 'IGHD3-22', 'IGHD6-6', 'IGHD2-15', 'IGHD2/OR15-2b', 
+               'IGHD5/OR15-5b')
 
-HUMAN_IGKV <- c('IGKV1-12', 'IGKV1-13', 'IGKV1-16', 'IGKV1-17', 'IGKV1-27', 'IGKV1-33', 'IGKV1-39', 'IGKV1-5', 'IGKV1-6', 
-                'IGKV1-8', 'IGKV1-9', 'IGKV1-NL1', 'IGKV1/OR-2', 'IGKV1/OR-3', 'IGKV1/OR-4', 'IGKV1/OR1-1', 'IGKV1/OR10-1', 
-                'IGKV1/OR15-118', 'IGKV1/OR2-0', 'IGKV1/OR2-1', 'IGKV1/OR2-108', 'IGKV1/OR2-11', 'IGKV1/OR2-118', 'IGKV1/OR2-2',
-                'IGKV1/OR2-3', 'IGKV1/OR2-9', 'IGKV1/OR22-5', 'IGKV1/OR9-1', 'IGKV1/OR9-2', 'IGKV1/ORY-1', 'IGKV1D-12', 'IGKV1D-13', 
-                'IGKV1D-16', 'IGKV1D-17', 'IGKV1D-33', 'IGKV1D-39', 'IGKV1D-42', 'IGKV1D-43', 'IGKV1D-8', 'IGKV2-18', 'IGKV2-24', 'IGKV2-28', 
-                'IGKV2-30', 'IGKV2-4', 'IGKV2-40', 'IGKV2/OR2-7D', 'IGKV2/OR22-4', 'IGKV2D-18', 'IGKV2D-24', 'IGKV2D-26', 'IGKV2D-28', 
-                'IGKV2D-29', 'IGKV2D-30', 'IGKV2D-40', 'IGKV3-11', 'IGKV3-15', 'IGKV3-20', 'IGKV3-7', 'IGKV3-NL1', 'IGKV3-NL2', 'IGKV3-NL3', 
-                'IGKV3-NL4', 'IGKV3-NL5', 'IGKV3/OR2-268', 'IGKV3D-11', 'IGKV3D-15', 'IGKV3D-20', 'IGKV3D-7', 'IGKV4-1', 'IGKV5-2', 'IGKV6-21', 
-                'IGKV6D-21', 'IGKV6D-41', 'IGKV7-3')
-HUMAN_IGKJ <- c('IGKJ1', 'IGKJ2', 'IGKJ3', 'IGKJ4', 'IGKJ5')
+HUMAN_IGHJ = c('IGHJ4', 'IGHJ1', 'IGHJ6', 'IGHJ3', 'IGHJ2', 'IGHJ5')
 
-HUMAN_IGLV <- c('IGLV1-36', 'IGLV1-40', 'IGLV1-41', 'IGLV1-44', 'IGLV1-47', 'IGLV1-50', 'IGLV1-51', 'IGLV10-54', 'IGLV11-55', 'IGLV2-11', 'IGLV2-14', 
-                'IGLV2-18', 'IGLV2-23', 'IGLV2-33', 'IGLV2-34', 'IGLV2-5', 'IGLV2-8', 'IGLV2-NL1', 'IGLV3-1', 'IGLV3-10', 'IGLV3-12', 'IGLV3-13', 'IGLV3-16', 
-                'IGLV3-19', 'IGLV3-21', 'IGLV3-22', 'IGLV3-25', 'IGLV3-27', 'IGLV3-31', 'IGLV3-9', 'IGLV4-3', 'IGLV4-60', 'IGLV4-69', 'IGLV5-37', 'IGLV5-39', 
-                'IGLV5-45', 'IGLV5-48', 'IGLV5-52', 'IGLV6-57', 'IGLV7-43', 'IGLV7-46', 'IGLV8-61', 'IGLV9-49')
-HUMAN_IGLJ <- c('IGLJ1', 'IGLJ2', 'IGLJ3', 'IGLJ4', 'IGLJ5', 'IGLJ6', 'IGLJ7')
+
+HUMAN_IGHV = c('IGHV4-4', 'IGHV7-40', 'IGHV2-5', 'IGHV4-28', 'IGHV6-1', 'IGHV3/OR16-6', 
+               'IGHV1-69D', 'IGHV1-2', 'IGHV5-51', 'IGHV2/OR16-5', 'IGHV3-30-52', 
+               'IGHV3-53', 'IGHV3/OR16-13', 'IGHV3-74', 'IGHV1-69-2', 'IGHV3-22', 
+               'IGHV4-34', 'IGHV1-45', 'IGHV3-35', 'IGHV3-64', 'IGHV4-61', 
+               'IGHV3-47', 'IGHV7-34-1', 'IGHV4-59', 'IGHV3-19', 'IGHV3-15', 
+               'IGHV4-39', 'IGHV2-70D', 'IGHV3-20', 'IGHV1-18', 'IGHV3-30-22', 
+               'IGHV4-38-2', 'IGHV3-54', 'IGHV5-10-1', 'IGHV3-33', 'IGHV3-21', 
+               'IGHV3-7', 'IGHV1/OR15-2', 'IGHV4-31', 'IGHV3/OR16-8', 'IGHV3-23D', 
+               'IGHV3-30-42', 'IGHV3-62', 'IGHV3-23', 'IGHV1-8', 'IGHV3/OR16-15', 
+               'IGHV1/OR15-1', 'IGHV3-43D', 'IGHV4-30-2', 'IGHV3-NL1', 'IGHV3-64D', 
+               'IGHV3-13', 'IGHV3-52', 'IGHV3-11', 'IGHV3-30-33', 'IGHV1/OR15-4', 
+               'IGHV5-78', 'IGHV4-55', 'IGHV3-16', 'IGHV3-33-2', 'IGHV7-4-1', 
+               'IGHV2-26', 'IGHV4-30-4', 'IGHV1/OR21-1', 'IGHV1/OR15-9', 'IGHV3/OR15-7', 
+               'IGHV3-9', 'IGHV3-30', 'IGHV3-29', 'IGHV3-38', 'IGHV2-70', 
+               'IGHV1-NL1', 'IGHV3-30-5', 'IGHV1/OR15-5', 'IGHV1-3', 'IGHV7-81', 
+               'IGHV3/OR16-14', 'IGHV1-68', 'IGHV3-63', 'IGHV3/OR16-10', 'IGHV4/OR15-8', 
+               'IGHV1-58', 'IGHV3-25', 'IGHV3-69-1', 'IGHV1-38-4', 'IGHV3/OR16-16', 
+               'IGHV1-69', 'IGHV3-32', 'IGHV1-46', 'IGHV1-24', 'IGHV3/OR16-9', 
+               'IGHV3-38-3', 'IGHV3-30-2', 'IGHV3-48', 'IGHV1/OR15-3', 'IGHV3-30-3', 
+               'IGHV3-73', 'IGHV3-49', 'IGHV3-66', 'IGHV3-43', 'IGHV2-10', 
+               'IGHV3/OR16-12', 'IGHV3-71', 'IGHV3-72')
+
+HUMAN_IGKJ = c('IGKJ5', 'IGKJ4', 'IGKJ3', 'IGKJ2', 'IGKJ1')
+
+HUMAN_IGKV = c('IGKV2-30', 'IGKV1-17', 'IGKV1-5', 'IGKV1/OR1-1', 'IGKV1/OR2-11', 'IGKV2D-30', 
+               'IGKV1-9', 'IGKV2-40', 'IGKV6D-41', 'IGKV1D-43', 'IGKV3-7', 
+               'IGKV6D-21', 'IGKV2-29', 'IGKV1/OR2-1', 'IGKV1-13', 'IGKV3D-20', 
+               'IGKV1-NL1', 'IGKV1D-12', 'IGKV3D-7', 'IGKV1/OR10-1', 'IGKV1/OR2-108', 
+               'IGKV1D-16', 'IGKV1/OR2-118', 'IGKV1-39', 'IGKV1/OR2-9', 'IGKV1D-17', 
+               'IGKV1/ORY-1', 'IGKV1/OR-4', 'IGKV1D-8', 'IGKV1D-42', 'IGKV2D-24', 
+               'IGKV1-27', 'IGKV1/OR15-118', 'IGKV1/OR22-5', 'IGKV1/OR9-1', 'IGKV2/OR2-7D', 
+               'IGKV2D-18', 'IGKV1/OR-3', 'IGKV1/OR9-2', 'IGKV1D-37', 'IGKV1D-39', 
+               'IGKV2D-29', 'IGKV5-2', 'IGKV2D-28', 'IGKV3/OR2-268', 'IGKV2D-26', 
+               'IGKV3-20', 'IGKV1-8', 'IGKV2D-40', 'IGKV6-21', 'IGKV3-11', 
+               'IGKV3-15', 'IGKV1/OR2-3', 'IGKV1-12', 'IGKV2/OR22-4', 'IGKV1/OR2-2', 
+               'IGKV1/OR-2', 'IGKV7-3', 'IGKV1/OR2-0', 'IGKV3D-11', 'IGKV1-16', 
+               'IGKV1D-33', 'IGKV1-33', 'IGKV3D-15', 'IGKV1D-13', 'IGKV2-18', 
+               'IGKV1-6', 'IGKV2-28', 'IGKV4-1', 'IGKV1-37', 'IGKV2-4', 
+               'IGKV2-24')
+
+HUMAN_IGLJ = c('IGLJ6', 'IGLJ5', 'IGLJ3', 'IGLJ4', 'IGLJ1', 'IGLJ7', 
+               'IGLJ2')
+
+HUMAN_IGLV = c('IGLV2-11', 'IGLV7-46', 'IGLV1-44', 'IGLV5-52', 'IGLV4-3', 'IGLV3-19', 
+               'IGLV3-27', 'IGLV2-18', 'IGLV2-8', 'IGLV3-21', 'IGLV2-33', 
+               'IGLV3-16', 'IGLV3-13', 'IGLV2-14', 'IGLV1-62', 'IGLV3-31', 
+               'IGLV3-9', 'IGLV4-60', 'IGLV1-51', 'IGLV7-43', 'IGLV5-45', 
+               'IGLV8-61', 'IGLV3-1', 'IGLV1-40', 'IGLV3-25', 'IGLV4-69', 
+               'IGLV9-49', 'IGLV5-39', 'IGLV1-47', 'IGLV2-NL1', 'IGLV2-34', 
+               'IGLV11-55', 'IGLV2-5', 'IGLV8/OR8-1', 'IGLV10-54', 'IGLV3-12', 
+               'IGLV3-22', 'IGLV1-41', 'IGLV5-37', 'IGLV5-48', 'IGLV1-36', 
+               'IGLV3-10', 'IGLV3-32', 'IGLV6-57', 'IGLV2-23', 'IGLV1-50')
 
 MOUSE_TRAV <- c('TRAV1', 'TRAV10', 'TRAV10D', 'TRAV10N', 'TRAV11', 'TRAV11D', 'TRAV11N', 'TRAV12-1', 'TRAV12-2', 'TRAV12-3', 'TRAV12D-1', 'TRAV12D-2', 
                 'TRAV12D-3', 'TRAV12N-1', 'TRAV12N-2', 'TRAV12N-3', 'TRAV13-1', 'TRAV13-2', 'TRAV13-3', 'TRAV13-4/DV7', 'TRAV13-5', 'TRAV13D-1', 
